f4e682283fe1ed882f0b6b8db71bbfceb60a6f1a
ANDREA LAURINO <andreajlaurino@gmail.com>
2023-06-28 16:37:45 -0300

    readme

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -19,12 +19,12 @@ const header = () => {
 						<img
 							alt="logo de instagram"
 							className="img-logo-lg"
-							src="src/assets/instagram-logo-17.png"
+							src="assets/instagram-logo-17.png"
 						/>
 						<img
 							alt="logo de instagram"
 							className="logo-md"
-							src="src/assets/instagram-logo-16.png"
+							src="assets/instagram-logo-16.png"
 						/>
 					</div>
 					<div className="header-icons">
